5f97fcd17525fc3628ee4f93100dabd81da5751a
Josh Conlin <joshjconlin@yahoo.com>
2020-06-11 12:14:38 -0600

    rework package for node/react projects instead of react-native. add additional git helper logic

--- index.js
+++ index.js
@@ -7,90 +7,95 @@ const readlineSync = require('readline-sync');
 const helpers = require('./lib/helpers');
 const log = require('./lib/log');
 
-
 const pathToRoot = process.cwd();
 const pathToPackage = argv.pathToPackage || `${pathToRoot}/package.json`;
-const pathToAppJson = `${pathToRoot}/app.json`;
 const info = helpers.getPackageInfo(pathToPackage);
 
-const pathToPlist = argv.pathToPlist || `${pathToRoot}/ios/${info.name}/Info.plist`;
-const pathToGradle = argv.pathToGradle || `${pathToRoot}/android/app/build.gradle`;
-
-// getting next version
 const versionCurrent = info.version;
 const versions = helpers.versions(versionCurrent);
-let major = helpers.version(versions[0], argv.major);
-let minor = helpers.version(versions[1], argv.minor, argv.major);
-let patch = helpers.version(versions[2], argv.patch, argv.major || argv.minor);
-let buildOption = argv.build || false;
-let jsBundle = argv.jsBundle || false;
 
-if (jsBundle) {
-  const appJson = require(pathToAppJson);
-  let version = parseInt(appJson.jsBundleIdentifier, 10);
-  version += 1;
-  helpers.changeJsBundleVersionInAppJson(pathToAppJson, version);
-  console.log(`jsBundleIdentifier changed in app.json to ${version}`);
-  return;
+const args = {
+  major: () => {
+    const major = !!(argv.major || argv.mj) || false;
+
+    return helpers.version(versions[0], major)
+  },
+  minor: () => {
+    const minor = argv.minor || argv.mn || false;
+
+    return helpers.version(versions[1], minor, args.major());
+  },
+  patch: () => {
+    const patch = argv.patch || argv.p || false;
+
+    return helpers.version(versions[2], patch, args.major() || args.minor());
+  },
+  message: (version) => {
+    if (argv.message || argv.m) {
+      return argv.message || argv.m;
+    }
+
+    return `"Release ${version}: increase versions and build numbers"`
+  },
+  tag: (version) => {
+    if (argv.tag || argv.t) {
+      return argv.tag || argv.t;
+    }
+
+    return `"v${version}"`;
+  },
+  version: () => `${args.major()}.${args.minor()}.${args.patch()}`
 }
 
-// getting next build number
-const buildCurrent = helpers.getBuildNumberFromPlist(pathToPlist);
-const build = buildCurrent + 1;
-
-if (buildOption) {
-  if (argv.ios) {
-    helpers.changeBuildInPlist(pathToPlist, build);
-    helpers.changeIosBuildNumberInAppJson(pathToAppJson, build);
-    console.log(`Build number in plist incremented from ${buildCurrent} to ${build}`);
-    return;
-  } else if (argv.android) {
-    helpers.changeBuildInGradle(pathToGradle, build);
-    helpers.changeAndroidVersionCodeInAppJson(pathToAppJson, build);
-    console.log(`Build number in gradle incremented from ${buildCurrent} to ${build}`);
-  } else {
-    helpers.changeBuildInPlist(pathToPlist, build);
-    helpers.changeBuildInGradle(pathToGradle, build);
-    helpers.changeIosBuildNumberInAppJson(pathToAppJson, build);
-    helpers.changeAndroidVersionCodeInAppJson(pathToAppJson, build);
-    console.log(`Build number incremented in plist and gradle from ${buildCurrent} to ${build}`);
-  }
-  return;
-}
-
-const version = `${major}.${minor}.${patch}`;
+// getting next version
+const version = args.version();
 
 // getting commit message
-const message = argv.m || argv.message || `release ${version}: increase versions and build numbers`;
+const message = args.message();
 
+// getting commit tag
+const commitTag = args.tag(version);
 
-log.info('\nI\'m going to increase the version in:');
-log.info(`- package.json (${pathToPackage});`, 1);
-log.info(`- ios project (${pathToPlist});`, 1);
-log.info(`- android project (${pathToGradle}).`, 1);
+// todo: display current branch, and ask if want to switch to master
 
-log.notice(`\nThe version will be changed:`);
-log.notice(`- from: ${versionCurrent} (${buildCurrent});`, 1);
-log.notice(`- to:   ${version} (${build}).`, 1);
+const initialize = new Promise((resolve, reject) => {
+  helpers
+      .ensureGitBranchClean()
+      .then(() => {
+        helpers.getGitBranch()
+            .then((branch) => {
+              return helpers.confirmBranch(branch)
+                  .then(resolve);
+            });
+      })
+      .catch(reject);
 
-if (version === versionCurrent) {
-  log.warning('\nNothing to change in the version. Canceled.');
-  process.exit();
-}
+  log.info('\nI\'m going to increase the version in:');
+  log.info(`- package.json (${pathToPackage});`, 1);
 
+  log.notice(`\nThe version will be changed:`);
+  log.notice(`- from: ${versionCurrent}`, 1);
+  log.notice(`- to:   ${version}`, 1);
 
-const chain = new Promise((resolve, reject) => {
-  log.line();
-
-  if (versions.length !== 3) {
-    log.warning(`I can\'t understand format of the version "${versionCurrent}".`);
+  if (version === versionCurrent) {
+    log.warning('\nNothing to change in the version. Canceled.');
+    process.exit();
   }
-
-  const question = log.info(`Use "${version}" as the next version? [y/n] `, 0, true);
-  const answer = readlineSync.question(question).toLowerCase();
-  answer === 'y' ? resolve() : reject('Process canceled.');
 });
 
+const chain = initialize.then(() => {
+  return new Promise((resolve, reject) => {
+    log.line();
+
+    if (versions.length !== 3) {
+      log.warning(`I can\'t understand format of the version "${versionCurrent}".`);
+    }
+
+    const question = log.info(`Use "${version}" as the next version? [y/n] `, 0, true);
+    const answer = readlineSync.question(question).toLowerCase();
+    answer === 'y' ? resolve() : reject('Process canceled.');
+  });
+});
 
 const update = chain.then(() => {
   log.notice('\nUpdating versions');
@@ -98,49 +103,108 @@ const update = chain.then(() => {
   log.info('Updating version in package.json...', 1);
 
   helpers.changeVersionInPackage(pathToPackage, version);
-  if (fs.existsSync(pathToAppJson)) {
-    helpers.changeVersionInAppJson(pathToAppJson, version);
-    helpers.changeIosBuildNumberInAppJson(pathToAppJson, build);
-    helpers.changeAndroidVersionCodeInAppJson(pathToAppJson, build);
-    log.success(`Version and builds in app.json changed.`, 2)
-  }
+
   log.success(`Version in package.json changed.`, 2);
-}).then(() => {
-  log.info('Updating version in xcode project...', 1);
-
-  helpers.changeVersionAndBuildInPlist(pathToPlist, version, build);
-  log.success(`Version and build number in ios project (plist file) changed.`, 2);
-}).then(() => {
-  log.info('Updating version in android project...', 1);
-
-  helpers.changeVersionAndBuildInGradle(pathToGradle, version, build);
-  log.success(`Version and build number in android project (gradle file) changed.`, 2);
 });
 
 const commit = update.then(() => {
-  log.notice(`\nI'm ready to cooperate with the git!`);
+  log.notice(`\nI'm ready to cooperate with git!`);
   log.info('I want to make a commit with message:', 1);
   log.info(`"${message}"`, 2);
-  log.info(`I want to add a tag:`, 1);
-  log.info(`"v${version}"`, 2);
 
-  const question = log.info(`Do you allow me to do this? [y/n] `, 1, true);
+  const question = log.info(`\nDo you want me to do this? [y/n] `, 1, true);
+
   const answer = readlineSync.question(question).toLowerCase();
+
   if (answer === 'y') {
     return helpers.commitVersionIncrease(version, message, [
       pathToPackage,
-      pathToPlist,
-      pathToGradle
     ]).then(() => {
-      log.success(`Commit with files added. Run "git push".`, 1);
+      log.success(`\nCommitted!\n`, 1);
+
+      return 'committed';
     });
   } else {
     log.warning(`Skipped.`, 1);
+
+    return 'skipped';
   }
 });
 
-commit.then(() => {
-  log.success(`\nDone!`);
+const pushCommit = commit.then((status) => {
+  if (status === 'committed') {
+    const question = log.info('I want to push this commit. Is this okay? [y/n]', 1);
+
+    const answer = readlineSync.question(question).toLowerCase();
+
+    if (answer === 'y') {
+      return helpers.pushCommit()
+          .then(() => {
+            log.success('\nCommit Pushed!\n');
+
+            return 'pushed'
+          });
+    } else {
+      log.info('\nCommit Skipped!\n', 1);
+
+      return 'skipped';
+    }
+  }
+
+  return 'skipped';
+});
+
+const tag = pushCommit.then((status) => {
+  if (status === 'pushed') {
+    log.info(`I want to add a tag:\n`, 1);
+    log.info(commitTag, 1)
+
+    const question = log.info(`\nDo you allow me to do this? [y/n]`);
+
+    const answer = readlineSync.question(question).toLowerCase();
+
+    if (answer === 'y') {
+      return helpers.tag(commitTag)
+          .then(() => {
+            log.success(`\nBranch Tagged!\n`);
+
+            return 'tagged';
+          });
+    } else {
+      log.info('\nTagging skipped\n');
+
+      return 'skipped';
+    }
+  }
+
+  return 'skipped';
+});
+
+const pushTag = tag.then((status) => {
+  if (status === 'tagged') {
+    const question = log.info(`\nDo you want me to push this tag? [y/n]`);
+
+    const answer = readlineSync.question(question).toLowerCase();
+
+    if (answer === 'y') {
+      return helpers.pushTag()
+          .then(() => {
+            log.info('\nTag pushed!\n');
+
+            return 'pushed';
+          });
+    } else {
+      log.info('\nPushing tag skipped\n', 1);
+
+      return 'skipped';
+    }
+  }
+
+  return 'skipped';
+});
+
+pushTag.then(() => {
+  log.success(`\nYou're all done!`);
 }).catch(e => {
   log.line();
   log.error(e)
--- readme.md
+++ readme.md
@@ -1,113 +1,81 @@
 # Install
-```npm install react-native-version-or-build-up```
+```npm install node-version-up```
 
-
-# React native version upper
-Increase `major`, `minor` or `patch` part of the version and build number in your app in package.json and in ios and android projects with one command.
-```
-node ./node_modules/react-native-version-or-build-up/index.js --patch -m 'commit message'
-```
+# Node/React version upper
+Increase `major`, `minor` or `patch` part of the version in your app in package.json with one command.
 
 With this script you can:
 - Increase `major`, `minor` or `patch` part in the version.
-- Increase `build` in `ios` or `android` or both by default. (i.e node ./node_modules/react-native-version-or-build-up/index.js --build --{ios/android}, or just --build for both);
 - Make a git commit with version changes.
 - Make a git tag with new version.
 
-## Example
-```bash
-> yarn run version:up -- --patch
-$ node ./node_modules/react-native-version-or-build-up/index.js "--patch"
-
-I'm going to increase the version in:
-  - package.json (./package.json);
-  - ios project (./ios/happinesstracker/Info.plist);
-  - android project (./android/app/build.gradle).
-
-The version will be changed:
-  - from: 0.2.2 (9);
-  - to:   0.2.3 (10).
-
-Use "0.2.3" as the next version? [y/n] y
-
-Updating versions
-  Updating version in package.json...
-    Version in package.json changed.
-  Updating version in xcode project...
-    Version and build number in ios project (plist file) changed.
-  Updating version in android project...
-    Version and build number in android project (gradle file) changed.
-
-I'm ready to cooperate with the git!
-  I want to make a commit with message:
-    "release 0.2.3: increase versions and build numbers"
-  I want to add a tag:
-    "v0.2.3" -m "release 0.2.3: increase versions and build numbers"
-  Do you allow me to do this? [y/n] y
-  Commit with files added. Run "git push".
-
-Done!
-```
-
-## Installation
-```
-yarn add react-native-version-or-build-up
-```
-
-Or via npm:
-```
-npm install react-native-version-or-build-up --save
-```
-
 ## Usage
-**1. Add command in the section `scripts` in the `package.json`**
-```json
-{
-  "name": "your-project-name",
-  "scripts": {
-    "version:up": "node ./node_modules/react-native-version-or-build-up/index.js"
-  }
-}
-```
-
-**2. Make sure you have defined the version**
+**1. Make sure you have defined the version and script**
 ```json
 {
   "name": "your-project-name",
   "version": "1.0.0",
   "scripts": {
-    "version:up": "node ./node_modules/react-native-version-or-build-up/index.js"
+    "version-up": "node ./node_modules/node-version-up/index.js"
   }
 }
 ```
 
-**3. Commit the package.json (optional)**
-```bash
-git add package.json
-git commit -m 'version:up command added'
+**2. Run version up.**
+```
+> npm run version-up --{major | minor | patch}
 ```
 
-**4. Increase version when needed**
+## Example
 ```bash
-yarn run version:up -- --major
+> npm run version-up --patch
+
+I'm going to increase the version in:
+  - package.json (./package.json);
+
+The version will be changed:
+  - from: 0.2.2
+  - to:   0.2.3
+
+Use "0.2.3" as the next version? [y/n] y
+
+Updating version
+  Updating version in package.json...
+    Version in package.json changed.
+
+I'm ready to cooperate with git!
+  I want to make a commit with message:
+    "Release 0.2.3: increase version"
+  Do you want me to do this? [y/n] y
+
+  Committed!
+
+  I want to push this commit. Is this okay? [y/n] y
+
+  Commit Pushed!    
+  
+  I want to add a tag:
+    "v0.2.3" -m "release 0.2.3: increase versions and build numbers"
+  Do you allow me to do this? [y/n] y
+  
+  Tagged!
+
+  Do you want me to push this tag? [y/n] y
+
+  Tag pushed! You're all done!
 ```
 
-Or via npm:
-```bash
-npm run version:up -- --major
-```
 ## Options
 You can pass option name and value with following syntax (remember to put `--` before options):
 ```
-yarn run version:up -- --flag value
+npm run version-up --flag value
 ```
 
 | **Option** | **Type** | **Default value** | **Description** |
 |------------|----------|-------------------|-----------------|
-| **`--major`** | `flag` | | Increase `major` version:<br/>**0**.0.0 -> **1**.0.0 |
-| **`--minor`** | `flag` | | Increase `minor` version:<br/>0.**0**.0 -> 0.**1**.0 |
-| **`--patch`** | `flag` | | Increase `patch` version:<br/>0.0.**0** -> 0.0.**1** |
-| **`--message` or `-m`** | `string` | `"release ${version}: increase versions and build numbers"` | Custom commit message. |
+| **`--major` or `--mj`** | `flag` | | Increase `major` version:<br/>**0**.0.0 -> **1**.0.0 |
+| **`--minor or `--mn`** | `flag` | | Increase `minor` version:<br/>0.**0**.0 -> 0.**1**.0 |
+| **`--patch or `--p`** | `flag` | | Increase `patch` version:<br/>0.0.**0** -> 0.0.**1** |
+| **`--message` or `--m`** | `string` | `"release ${version}: increase versions and build numbers"` | Custom commit message. |
+| **`--tag` or `--t`** | `string` | `v{major}.{minor}.{patch}` | Custom git tag |
 | **`--pathToPackage './path'`** | `string` | `./package.json` | Path to `package.json` file in your project. |
-| **`--pathToPlist './path'`** | `string` | `./ios/${package.name}/Info.plist` | Path to `Info.plist` file (ios project). |
-| **`--pathToGradle './path'`** | `string` | `./android/app/build.gradle` | Path to `build.gradle` file (android project). |
