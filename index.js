3978241f543f8567961defb1f7ad72ad3bb9a5fb
Josh Conlin <joshjconlin@yahoo.com>
2020-06-15 15:52:10 -0600

    rewrite in progress

--- index.js
+++ index.js
@@ -1,166 +1,92 @@
 'use strict';
 
-const argv = require('yargs').argv;
-// todo: update yargs
 const readlineSync = require('readline-sync');
 
-const helpers = require('./lib/helpers');
+const utils = require('./lib/utils');
 const log = require('./lib/log');
 const constants = require('./lib/constants');
 
-const pathToRoot = process.cwd();
-const pathToPackage = argv.pathToPackage || `${pathToRoot}/package.json`;
-const info = helpers.getPackageInfo(pathToPackage);
+const {
+    commitTag,
+    message,
+    shouldForceGit,
+    version,
+    noChange,
+    pathToPackage
+} = require('./lib/args');
 
-const versionCurrent = info.version;
-const versions = helpers.versions(versionCurrent);
-
-
-const args = {
-    hasMajor: () => argv.major || argv.mj || false,
-    hasMinor: () => argv.minor || argv.mn || false,
-    hasPatch: () => argv.patch || argv.p || false,
-    major: () => {
-        const major = argv.major || argv.mj || false;
-
-        return helpers.version(versions[0], major, false, 'major');
+const {
+    messages: {
+        dialog,
+        answers,
+        questions,
+        errors,
     },
-    minor: (major) => {
-        const minor = argv.minor || argv.mn || false;
+    enums,
+    supportedFormatLengths,
+    currentFormat,
 
-        return helpers.version(versions[1], minor, major);
-    },
-    patch: (major, minor) => {
-        const patch = argv.patch || argv.p || false;
+} = constants;
 
-        return helpers.version(versions[2], patch, major || minor, 'patch');
-    },
-    message: (version) => {
-        if (argv.message || argv.m) {
-            return argv.message || argv.m;
-        }
-
-        return `Release ${version}: increase version.`
-    },
-    tag: (version) => {
-        if (argv.tag || argv.t) {
-            return argv.tag || argv.t;
-        }
-
-        return `"v${version}"`;
-    },
-    version: (major, minor, patch) => `${major}.${minor}.${patch}`,
-    force: () => {
-        return !!(argv.force || argv.f);
-    }
-}
-
-const major = args.major();
-const minor = args.minor(args.hasMajor());
-const patch = args.patch(args.hasMajor(), args.hasMinor());
-
-// getting next version
-const version = args.version(major, minor, patch);
-
-// getting commit message
-const message = args.message(version);
-
-// getting commit tag
-const commitTag = args.tag(version);
-
-const force = args.force();
 
 const initialize = new Promise((resolve, reject) => {
-    log.line();
-    log.success('Welcome to Node Version Up!');
-    log.line();
+    utils.runDialog('welcome');
 
-    helpers
-        .ensureGitBranchClean(force)
-        .then(() => {
-            helpers.getGitBranch()
-                .then((branch) => {
-                    return helpers.confirmBranch(branch)
-                        .then(resolve).catch(reject);
-                });
-        })
+    utils
+        .ensureGitBranchClean(shouldForceGit)
+        .then(utils.getGitBranch)
+        .then(utils.confirmBranch)
+        .then(resolve)
         .catch(reject);
 
-    log.warning('I\'m going to increase the application version in:');
-    log.notice(`- package.json (${pathToPackage});`, 1);
-
-    log.warning(`\nThe version will be changed:`);
-    log.notice(`- from: ${versionCurrent}`, 1);
-    log.notice(`- to:   ${version}`, 1);
-    log.line();
-
-    if (version === versionCurrent) {
-        log.warning('Nothing to change in the version. Canceled.');
-        process.exit();
+    if (noChange) {
+        utils.error('noChange');
     }
 });
 
-const chain = initialize.then(() => {
+const confirmVersionChange = initialize.then(() => {
     return new Promise((resolve, reject) => {
-        if (versions.length !== 3) {
-            log.warning(`I can\'t understand format of the version "${versionCurrent}". Process Failed`);
-            return reject('Process canceled.') // todo: error codes/messages
-        }
+        utils.isVersionCorrectFormat(version, constants.currentFormat, reject);
 
-        const question = log.danger(`Use "${version}" as the next version? [y/n] `, 1, true);
-        const answer = readlineSync.question(question).toLowerCase();
+        utils.runDialog('displayVersionInfo');
 
-        log.line();
-
-        // todo: status in resolve
-        return answer === 'y' ? resolve() : reject('Process canceled.');
+        return utils.handleAnswer(
+            utils.handleQuestion(questions.useVersion),
+            enums.answers.confirm,
+            resolve,
+            reject,
+            errors.cancelled,
+        );
     });
 });
 
-// get dialog()
+const updateVersion = confirmVersionChange.then(() => {
+    utils.runDialog('updateInProcess');
 
-const update = chain.then(() => {
-    log.notice('Updating version in package.json...', 0);
-    log.line();
+    utils.changeVersionInPackage(pathToPackage, version);
 
-    helpers.changeVersionInPackage(pathToPackage, version);
-
-    log.success(`Version in package.json changed successfully!`, 1);
-
-    log.line();
+    utils.runDialog('updateSuccess');
 });
 
-const commit = update.then(() => {
-    log.success(`I'm ready to cooperate with git!`);
+const commit = updateVersion.then(() => {
+    utils.runDialog('initializeGit');
 
-    log.line();
-
-    log.warning('I want to make a commit with message:', 1);
-
-    log.notice(`"${message}"`, 2);
-
-    const question = log.danger(`Is that ok? [y/n] `, 1, true);
-    log.line();
-
-    const answer = readlineSync.question(question).toLowerCase();
-
-    if (answer === 'y') {
-        return helpers.commitVersionIncrease(version, message, [
-            pathToPackage,
-        ], force).then(() => {
-            log.line();
-            log.success(`Committed!`, 1);
-            log.line();
-
-            return 'committed';
-        });
-    } else {
-        log.warning(`Skipped.`, 1);
-
-        return 'skipped';
-    }
+    // todo: add manual paths with flags, more git support
+    return utils.handleAnswer(
+      utils.handleQuestion(questions.confirmAction),
+      enums.answers.confirm,
+        () => utils.commitVersionIncrease(
+            version,
+            message,
+            [pathToPackage],
+            shouldForceGit,
+        ).then(() => utils.runDialog('committed') || enums.statuses.committed),
+        () => enums.statuses.skipped
+    );
 });
 
+
+// todo: withStatusCheck
 const pushCommit = commit.then((status) => {
     if (status === 'committed') {
         log.warning('I want to push this commit.', 1);
@@ -174,7 +100,7 @@ const pushCommit = commit.then((status) => {
         log.line();
 
         if (answer === 'y') {
-            return helpers.pushCommit()
+            return utils.pushCommit()
                 .then(() => {
                     log.success('Commit Pushed!', 1);
                     log.line();
@@ -205,7 +131,7 @@ const tag = pushCommit.then((status) => {
         log.line();
 
         if (answer === 'y') {
-            return helpers.tag(commitTag)
+            return utils.tagBranch(commitTag)
                 .then(() => {
                     log.success(`Branch Tagged!`, 1);
                     log.line();
@@ -232,7 +158,7 @@ const pushTag = tag.then((status) => {
         log.line();
 
         if (answer === 'y') {
-            return helpers.pushTag()
+            return utils.pushTag()
                 .then(() => {
                     log.success('Tag pushed!', 1);
 
